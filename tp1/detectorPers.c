51ba4ce12b8f9586bc359ba592da6b889d71894d
haguilerts <haguilerts@hotmail.com>
2020-10-26 23:43:30 -0300

    tp terminado, falta modularizar, PRE y POST

--- tp1/detectorPers.c
+++ tp1/detectorPers.c
@@ -17,11 +17,16 @@ const char Amargo= 'A';
 #define POLLITO     'P'
 #define BELLOTA     'B'
 #define BLUE        'R'
-#define JHONNY_BRAVO'J'
+#define JHONNY_BRAVO 'J'
 #define PURO_HUESOS 'H'
 
+#define FECHA_NACIENTO 'F'
 
-  int altura_personaje(char p){
+/*
+   *PRE:
+   *POST:
+   */
+int altura_personaje(char p){
     int alt;
     switch (p){
       case CORAJE:
@@ -45,47 +50,36 @@ const char Amargo= 'A';
     }
     return  alt;
   }
-  /*
+   /*
    *PRE:
    *POST:
    */
-  int puntaje_fechaNaci(int *f){    
-      do{
-        printf("ingrese la fecha de nacimiento entre [1988 -2008], excluyente\n");
-        scanf("%i",f);
-      }while(*f<1988 || *f>2008); // 
-      int res=((*f) % 20)+1;
-        printf("res:%i \n",res);
-        //printf("MisRes:%i \n",1988 % 20);
-      return res;
-  } 
-  int puntaje_sabor(char *s){
-    do{
-      printf("ingrese un Sabor: Dulce(D),Salado(S) o Amargo(A)\n");
-      scanf("%c",s);
-    } while((*s)!=Dulce && (*s)!=Salado && (*s)!=Amargo);
-    int puntaje=20;    
-    if (*s == Dulce)
-      puntaje=15;
-    else if(*s == Salado)
-      puntaje=5;
-    return puntaje;
-  }
-  int puntaje_Zapato(int *tz){
-    do{
-      printf("ingrese el Talle de Zapato entre [33 - 47], excluyente o '0'\n");
-      scanf("%i",tz);
-    }while((*tz<33 || *tz>47) && *tz!=0);
-    int cont=0;
-      if (*tz >=33 && *tz <=37)
-        cont=2;
-      if (*tz >=38 && *tz <=42)
-        cont=3;            
-      else if (*tz >=43 && *tz <=47)
-        cont=4;        
-    printf("c: %i\n",cont);
-    return cont;   
-  }
+  void traerNombre(int jug1){    
+    switch (jug1){
+    case 30:
+        printf("- Coraje - Es el personaje detectado por la ULTRA MAQUINA DETECTADORA.");
+      break;
+    case 50:
+        printf("- Pollito - Es el personaje detectado por la ULTRA MAQUINA DETECTADORA.");
+      break;
+    case 120:
+        printf("- Bellota - Es el personaje detectado por la ULTRA MAQUINA DETECTADORA.");
+      break;
+    case 140:
+        printf("- Blue - Es el personaje detectado por la ULTRA MAQUINA DETECTADORA.");
+      break;
+    case 180:
+        printf("- Jhony_Bravo - Es el personaje detectado por la ULTRA MAQUINA DETECTADORA.");
+      break;
+    case 200:
+        printf("- Puro_huesos - Es el personaje detectado por la ULTRA MAQUINA DETECTADORA.");
+      break;
+    }
+   }  
+   /*
+   *PRE:
+   *POST:
+   */
   int puntaje_color(char *c){  
     int res;    
     do{
@@ -110,64 +104,152 @@ const char Amargo= 'A';
         case AMARRILLO:
             res=5;          
           break;
-        default:
-            
-          break;
-        printf("res: %i\n",res);
+        //printf("res: %i\n",res);
       }
     } while (*c!=AZUL && *c!=AMARRILLO && *c!=VERDE && *c!=NEGRO && *c!=BLANCO && *c!=ROSA );
     return res;    
   }
+  
+  //-----------------------------------------------------------------------------------------------------
+  int operacion_de_puntaje(int *f){
+     int res=((*f) % 20)+1;
+     return res;
+  }
+  /*
+   *PRE: se debe introducir un valor entero entre [1988 -2008], excluyente
+   *POST: retornara una operacion 
+   */
+  int introducir_fNacimiento(int *f){    
+      do{
+        printf("ingrese la fecha de nacimiento entre [1988 -2008], excluyente\n");
+        scanf("%i",f);
+      }while(*f<1988 || *f>2008 ); // 
+       return operacion_de_puntaje(f);
+        //printf("resOperacion:%i \n",res);
+        //printf("MisRes:%i \n",1988 % 20);
+      
+  } 
+   /*
+   *PRE:
+   *POST:
+   */
+  int puntaje_sabor(char *s){
+    do{
+       // printf("123\n");
+      printf("ingrese un Sabor: Dulce(D),Salado(S) o Amargo(A)\n");
+       // printf("456\n");
+      scanf("%c",s);
+       // printf("789\n");
+    } while(!( *s==Dulce || *s==Salado || *s==Amargo) );
+    int puntaje=20;    
+    if (*s == Dulce)
+      puntaje=15;
+    else if(*s == Salado)
+      puntaje=5;
+    return puntaje;
+  }
+  
+   /*
+   *PRE:
+   *POST:
+   */
+  int puntaje_Zapato(int *tz){
+    do{
+      printf("ingrese el Talle de Zapato entre [33 - 47], excluyente o '0'\n");
+      scanf("%i",tz);
+    }while((*tz<33 || *tz>47) && *tz!=0);
+    int cont=0;
+      if (*tz >=33 && *tz <=37)
+        cont=2;
+      if (*tz >=38 && *tz <=42)
+        cont=3;            
+      else if (*tz >=43 && *tz <=47)
+        cont=4;        
+    printf("c: %i\n",cont);
+    return cont;   
+  }
+  
+   /*
+   *PRE:
+   *POST:
+   */
   void puntaje_altura(int *a){
       do{
         printf("ingrese la Altura entre [10 - 250], excluyente\n");
         scanf("%i",&*a);
       }while(*a<10 || *a>250);    
   }
+   /*
+   *PRE:
+   *POST:
+   */
   int puntaje_total(int*s,int*z,int*c,int*f){
     return ((*s + *c + *f)*(*z));
   }
-  int personaje_indicado(p1,p2,alturaUsuario){
-    int jug1=abs(p1-alturaUsuario);
-    int jug2=abs(p2-alturaUsuario);
-    if (jug1<jug2){
-     return jug1;
-    return jug2;
-    
-  }
-  void personajes_posibles(puntajeTotal, alturaUsuario){
-    int p1,p2;
-    if (puntajeTotal>=1 && puntajeTotal<=80){
-      p1=altura1=altura_personaje(JHONNY_BRAVO);
-      p2=altura1=altura_personaje(CORAJE);      
-    }else if(puntajeTotal>=81 && puntajeTotal<=160){
-      p1=altura1=altura_personaje(PURO_HUESOS);
-      p2=altura1=altura_personaje(POLLITO); 
-    }else if(puntajeTotal>=161 && puntajeTotal<=240){
-      p1=altura1=altura_personaje(BLUE);
-      p2=altura1=altura_personaje(BELLOTA);
+
+   /*
+   *PRE:
+   *POST:
+   */
+  void personaje_indicado(int p1, int p2, int* alturaUsuario){
+    int jug1=abs(p1-(*alturaUsuario));
+    int jug2=abs(p2-(*alturaUsuario));
+    printf("j1: %i  -- j2: %i \n",jug1,jug2);
+    if (jug1<jug2)
+      traerNombre(p1);     
+    else
+      traerNombre(p2);   
+  } 
+   /*
+   *PRE:
+   *POST:
+   */
+  void personajes_posibles(int punTotal, int* alturaUsuario){
+    int altPersonaje1;
+    int altPersonaje2;
+
+    if (punTotal>=1 && punTotal<=80){
+      altPersonaje1=altura_personaje(JHONNY_BRAVO);
+      altPersonaje2=altura_personaje(CORAJE);      
+    }else if(punTotal>=81 && punTotal<=160){
+      altPersonaje1=altura_personaje(PURO_HUESOS);
+      altPersonaje2=altura_personaje(POLLITO); 
+    }else{
+      //if(punTotal>=161 && punTotal<=240){
+      altPersonaje1=altura_personaje(BLUE);
+      altPersonaje2=altura_personaje(BELLOTA);
     }
-    int jugIndicado=personaje_indicado(p1,p2,alturaUsuario);
-
-  }
-
-  void  ingresarDatos(){
-    int fecha,talleZapato,altura;
-    char sabor, color;
-    int fechaN =puntaje_fechaNaci(&fecha);
-    int preSabor  =puntaje_sabor(&sabor);
-    int zapato  =puntaje_Zapato(&talleZapato);
-    int col   =puntaje_color(&color);
-    puntaje_altura(&altura);      
-    int punTotal= puntaje_total(&preSabor,&zapato,&col,&fechaN);
-    personajes_posibles(punTotal, altura, )
+    personaje_indicado(altPersonaje1,altPersonaje2,alturaUsuario);  
+    printf("pt: %i\n",punTotal);
+   // printf("p1: %i  -- p2: %i \n",p1,p2);
+   // printf("jugadir indicado es: %i \n",jugIndicado);
     
-    printf("resultado es: %i",punTotal);
+  }
+   /*
+   *PRE:
+   *POST:
+   */
+  void  ingresarDatos(int* fecha,int* talleZapato,int* altura,char* sabor,char* color ){
+    int fechaN    =introducir_fNacimiento(fecha);
+    int preSabor  =puntaje_sabor(sabor);
+    int zapato    =puntaje_Zapato(talleZapato);
+    int col       =puntaje_color(color);
+    puntaje_altura(altura);      
+    int punTotal  =(preSabor + col + fechaN) * zapato;
+    personajes_posibles(punTotal, altura);
+    
+    //printf("resultado es: %i",punTotal);
   }
 //=========================================================================
 int main(){
-    ingresarDatos();
-    //puntaje_total=(puntaje_comida()+puntaje_color()+puntaje_año() ) * mult_talla_zapato();
+    int fachaNacimiento;
+    int tallaZapato;
+    int alturaPers;
+    char saborPreferido;
+    char colorPreferido;
+    ingresarDatos(&fachaNacimiento,&tallaZapato,&alturaPers,&saborPreferido,&colorPreferido);
+    
+   
     return 0;
 }
 //gcc detectorPers.c -o e -Wall -Wconversion -Werror && ./e
